2a074a9a1b768f5c6c4396b2fc344a5104f68fac
taji341 <tajitaji341@gmail.com>
2022-06-25 12:39:47 +0900

    検索機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,8 @@ class ApplicationController < ActionController::Base
   before_action :set_q
 
   def search
-    @rooms = Room.all
-    @results = @q.result
+    @rooms = @q.result
   end
-
-  private
 
   def set_q
     @q = Room.ransack(params[:q])
